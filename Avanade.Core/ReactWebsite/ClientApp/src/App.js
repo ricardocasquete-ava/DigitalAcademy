2b7191adebaf1ee78e17442421768e4b1de0852c
Ricardo Casquete <ricardo.casquete@avanade.com>
2022-05-26 11:33:33 +1000

    added react site

---
+++ Avanade.Core/ReactWebsite/ClientApp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppRouting from './AppRouting';
+import AppLayout from './controls/AppLayout';
+
+const App = (props) => {
+
+    return (
+        <AppLayout>
+            <AppRouting />
+        </AppLayout>
+    )
+};
+export default App;
+
+
---
+++ Avanade.Core/ReactWebsite/ClientApp/src/AppRouting.js
@@ -0,0 +1,14 @@
+﻿import React, { Fragment } from 'react';
+import { Route } from 'react-router';
+
+import Landing from './components/Landing';
+
+const AppRouting = (props) => {
+
+    return (
+        <Fragment>
+            <Route match="/" component={Landing}/>
+        </Fragment>
+    );
+};
+export default AppRouting;
